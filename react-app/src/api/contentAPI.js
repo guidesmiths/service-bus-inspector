ddb677d23c37d0a53864e323cd4ab89288e48516
Daniel Herrero <danielherrerohernando@gmail.com>
2020-01-08 18:18:29 +0100

    other: fixing lint

--- react-app/src/api/contentAPI.js
+++ react-app/src/api/contentAPI.js
@@ -2,110 +2,110 @@ import axios from 'axios';
 import { handlingResponse, logError } from './utils';
 
 export const getDlq = params =>
-	axios({
-		method: 'post',
-		url: '/peekdlq',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: {
-			topic: params.payload.content.topic,
-			subscription: params.payload.content.sub,
-			numMessages: params.payload.content.numDlq
-		}
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/peekdlq',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: {
+      topic: params.payload.content.topic,
+      subscription: params.payload.content.sub,
+      numMessages: params.payload.content.numDlq
+    }
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const deleteDlq = params =>
-	axios({
-		method: 'post',
-		url: '/processdlq',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: {
-			topic: params.payload.content.topic,
-			subscription: params.payload.content.sub
-		}
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/processdlq',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: {
+      topic: params.payload.content.topic,
+      subscription: params.payload.content.sub
+    }
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const deleteActive = params =>
-	axios({
-		method: 'post',
-		url: '/deleteActive',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: {
-			topic: params.payload.content.topic,
-			subscription: params.payload.content.sub
-		}
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/deleteActive',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: {
+      topic: params.payload.content.topic,
+      subscription: params.payload.content.sub
+    }
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const getActive = params =>
-	axios({
-		method: 'post',
-		url: '/peekactive',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: {
-			nameSpace: params.payload.content.nameSpace,
-			topic: params.payload.content.topic,
-			subscription: params.payload.content.sub,
-			numMessages: params.payload.content.numActive
-		}
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/peekactive',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: {
+      nameSpace: params.payload.content.nameSpace,
+      topic: params.payload.content.topic,
+      subscription: params.payload.content.sub,
+      numMessages: params.payload.content.numActive
+    }
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const getToken = credentials =>
-	axios({
-		method: 'post',
-		url: '/auth',
-		headers: {},
-		data: {
-			clientId: credentials.payload.clientId,
-			clientSecret: credentials.payload.clientSecret,
-			appTenantId: credentials.payload.appTenantId,
-			subscriptionId: credentials.payload.subscriptionId
-		}
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/auth',
+    headers: {},
+    data: {
+      clientId: credentials.payload.clientId,
+      clientSecret: credentials.payload.clientSecret,
+      appTenantId: credentials.payload.appTenantId,
+      subscriptionId: credentials.payload.subscriptionId
+    }
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const getNamespaces = token =>
-	axios({
-		method: 'get',
-		url: '/namespaces',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: {}
-	});
+  axios({
+    method: 'get',
+    url: '/namespaces',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: {}
+  });
 
 export const getTopics = body =>
-	axios({
-		method: 'post',
-		url: '/namespace',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
-		data: body
-	})
-		.then(handlingResponse([200], 'Error trying to get content'))
-		.catch(logError);
+  axios({
+    method: 'post',
+    url: '/namespace',
+    headers: {
+      Authorization: localStorage.getItem('token')
+    },
+    data: body
+  })
+    .then(handlingResponse([200], 'Error trying to get content'))
+    .catch(logError);
 
 export const tokenHealth = token =>
-	axios({
-		method: 'get',
-		url: '/tokenhealth',
-		headers: {
-			Authorization: token
-		}
-	});
+  axios({
+    method: 'get',
+    url: '/tokenhealth',
+    headers: {
+      Authorization: token
+    }
+  });
